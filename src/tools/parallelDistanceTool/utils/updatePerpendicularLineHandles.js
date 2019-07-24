de3e57cfe806ed939b7caba862d4afc5a467e41f
Unknown <alejandra.cas.cru@gmail.com>
2019-07-24 09:39:41 -0500

    draw second line, rotate lines when the tool is created

---
+++ src/tools/parallelDistanceTool/utils/updatePerpendicularLineHandles.js
@@ -0,0 +1,72 @@
+// Update the  perpendicular line handles
+export default function(eventData, data) {
+  if (!data.handles.perpendicularStart.locked) {
+    return;
+  }
+
+  let startX_p1, startY_p1, endX_p1, endY_p1;
+  let startX_p2, startY_p2, endX_p2, endY_p2;
+
+  const { start, end } = data.handles;
+
+  if (start.x === end.x && start.y === end.y) {
+    startX_p1 = start.x;
+    startY_p1 = start.y;
+    endX_p1 = end.x;
+    endY_p1 = end.y;
+  } else {
+    // Mid point of long-axis line
+    const mid = {
+      x: (start.x + end.x) / 2,
+      y: (start.y + end.y) / 2,
+    };
+
+    // Length of long-axis
+    const dx = (start.x - end.x) * (eventData.image.columnPixelSpacing || 1);
+    const dy = (start.y - end.y) * (eventData.image.rowPixelSpacing || 1);
+    const length = Math.sqrt(dx * dx + dy * dy);
+
+    const baseline_angle = Math.abs(Math.atan2(dy, dx));
+    let perpenticular_angle, dx_perpenticular, dy_perpenticular;
+
+    if (baseline_angle > Math.PI / 2) {
+      perpenticular_angle = baseline_angle - Math.PI / 2;
+    } else {
+      perpenticular_angle = Math.PI / 2 - baseline_angle;
+    }
+
+    if (dy < 0) {
+      dx_perpenticular = length * Math.cos(perpenticular_angle);
+    } else {
+      dx_perpenticular = -1 * length * Math.cos(perpenticular_angle);
+    }
+
+    if (dx < 0) {
+      dy_perpenticular = -1 * length * Math.sin(perpenticular_angle);
+    } else {
+      dy_perpenticular = length * Math.sin(perpenticular_angle);
+    }
+
+    const vectorX = (start.x - end.x) / length;
+    const vectorY = (start.y - end.y) / length;
+    startX_p1 = start.x - (vectorX * length) / 4;
+    startY_p1 = start.y - (vectorY * length) / 4;
+    endX_p1 = startX_p1 + dx_perpenticular;
+    endY_p1 = startY_p1 + dy_perpenticular;
+
+    startX_p2 = start.x - (vectorX * length * 3) / 4;
+    startY_p2 = start.y - (vectorY * length * 3) / 4;
+    endX_p2 = startX_p2 + dx_perpenticular;
+    endY_p2 = startY_p2 + dy_perpenticular;
+  }
+
+  data.handles.perpendicularStart.x = startX_p1;
+  data.handles.perpendicularStart.y = startY_p1;
+  data.handles.perpendicularEnd.x = endX_p1;
+  data.handles.perpendicularEnd.y = endY_p1;
+
+  data.handles.perpendicularStart2.x = startX_p2;
+  data.handles.perpendicularStart2.y = startY_p2;
+  data.handles.perpendicularEnd2.x = endX_p2;
+  data.handles.perpendicularEnd2.y = endY_p2;
+}
