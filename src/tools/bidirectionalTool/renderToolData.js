49da57d7fed787b1c3bb410a9a4d13bd4d3151cf
Alejandra <alejandra.cas.cru@gmail.com>
2019-07-30 12:13:27 -0500

    fix handles active bug

--- src/tools/bidirectionalTool/renderToolData.js
+++ src/tools/bidirectionalTool/renderToolData.js
@@ -90,7 +90,7 @@ export default function(evt) {
       };
 
       // Draw the handles
-      drawHandles(context, eventData, data.handles, handleOptions);
+      drawHandles(context, eventData, data.handles, color, handleOptions);
 
       // Draw the textbox
       // Move the textbox slightly to the right and upwards
